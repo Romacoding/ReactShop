24bcdf34dcaf0fedb055213b4bde4c42ed954b04
Roman Ostash <roman.ostash@stots.edu>
2020-05-10 14:11:27 -0400

    Added .env file. Changed base Url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://reactshop1c.herokuapp.com"
+    ? "https://still-headland-89421.herokuapp.com/"
     : "http://localhost:3000";
 
 export default baseUrl;
